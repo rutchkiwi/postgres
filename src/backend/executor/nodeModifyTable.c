4696f15bbc228ad6aa23fb69b2f3e97641610366
Viktor Holmberg <v@viktorh.net>
2025-11-20 20:18:00 +0100

    ON CONFLCIT DO SELECT: More review fixes
    - Docs updated in a bunch of forgotten places
    
    - Test diff made smaller against master
    
    - ExecOnConflictSelect minor refactor:
    	- Invert if statment to avoid negation
    	- Add comment for the LCS_NONE case
    	- Remove the switch default case, make compiler check all possible cases
    	- Improve some comments
    	- Give CMD_INSERT to ExecProcessReturning

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3023,8 +3023,13 @@ ExecOnConflictSelect(ModifyTableContext *context,
 	 */
 	Assert(!resultRelInfo->ri_needLockTagTuple);
 
-	/* Lock or fetch the existing tuple to select */
-	if (lockStrength != LCS_NONE)
+	if (lockStrength == LCS_NONE)
+	{
+		if (!table_tuple_fetch_row_version(relation, conflictTid, SnapshotAny, existing))
+			/* The pre-existing tuple was deleted */
+			return false;
+	}
+	else
 	{
 		LockTupleMode lockmode;
 
@@ -3042,20 +3047,16 @@ ExecOnConflictSelect(ModifyTableContext *context,
 			case LCS_FORUPDATE:
 				lockmode = LockTupleExclusive;
 				break;
-			default:
-				elog(ERROR, "unexpected lock strength %d", lockStrength);
+			case LCS_NONE:
+				/* Prevent a compiler warning */ 
+				pg_unreachable();
 		}
 
 		if (!ExecOnConflictLockRow(context, existing, conflictTid,
 								   resultRelInfo->ri_RelationDesc, lockmode, false))
 			return false;
 	}
-	else
-	{
-		if (!table_tuple_fetch_row_version(relation, conflictTid, SnapshotAny, existing))
-			return false;
-	}
-
+	
 	/*
 	 * For the same reasons as ExecOnConflictUpdate, we must verify that the
 	 * tuple is visible to our snapshot.
@@ -3097,11 +3098,12 @@ ExecOnConflictSelect(ModifyTableContext *context,
 							 mtstate->ps.state);
 	}
 
-	/* Parse analysis should already have disallowed this */
+	/* Parse analysis should already have disallowed this, as RETURNING 
+	 * is required for DO SELECT.
+	 */
 	Assert(resultRelInfo->ri_projectReturning);
 
-	/* Process RETURNING like an UPDATE that didn't change anything */
-	*rslot = ExecProcessReturning(context, resultRelInfo, CMD_UPDATE,
+	*rslot = ExecProcessReturning(context, resultRelInfo, CMD_INSERT,
 								  existing, existing, context->planSlot);
 
 	if (canSetTag)
